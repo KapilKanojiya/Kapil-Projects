93b1477962a4ba51030aaf92b200f682a8df73bc
kapil kanojiya <kanojiyakapil@gmail.com>
2022-09-22 23:46:37 +0530

    Bank System commit

--- StaticBankSystem.java
+++ StaticBankSystem.java
@@ -1,22 +1,3 @@
-//Assignment-8 : 16 June 2020
-//Banking System
-
-// Create a Banking System which has following functionality.
-
-// Create two object to perform below scenario.
-// 1) Method to debit amount
-// 2) Method to credit amount
-// 3) Method to printBalance [it will print current balance]
-// 4) individualTransactionSummary() method should show how many times individually debit, credit & printBalance method called .
-// Hint : Create 3 non static instance variable.
-// Output:
-// user1 transaction summary : Credit - 2 times, Debit - 1 times, printBalance - 1 time
-// user2 transaction summary : Credit - 5 times, Debit - 2 times, printBalance - 0 time
-
-// 5) allTransactionSummary() method should show by both users total how many times debit, credit & printBalance method get called.
-// Hint : Create 3 static variable will be required.
-// Output:
-// All transaction summary : Credit - 7 times, Debit - 3 times, printBalance - 1 time 
 
 package assignments;
 
@@ -102,6 +83,7 @@ public class StaticBankSystem {
 		staticbanksystem2.setInitialBalance(20000);
 		staticbanksystem2.debitAmount(3000);
 		staticbanksystem2.creditAmount(2000);
+		staticbanksystem2.creditAmount(1000);
 		staticbanksystem2.printBalance();
 		staticbanksystem2.individualTransactionSummary();
 		staticbanksystem2.allTransactionSummary();
